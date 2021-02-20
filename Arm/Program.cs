f9dd1c01bc4af65deef4bff617392da25f194386
Nathan Stiles <stiles.nathan@outlook.com>
2021-02-20 16:25:34 -0500

    arm

---
+++ Arm/Program.cs
@@ -0,0 +1,203 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VRage;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        readonly GTS gts;
+        readonly Logger g;
+        readonly IMyTextPanel lcd;
+
+        readonly Finger firstFinger;
+        Finger lastFinger;
+        readonly List<IMyMotorAdvancedStator> controlRotors = new List<IMyMotorAdvancedStator>();
+        readonly List<Finger> fingers = new List<Finger>();
+        bool walkComplete = false;
+        readonly IMyCockpit control;
+
+        public Program() {
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            g = new Logger();
+            gts = new GTS(this, g);
+            gts.getByTag("control", ref lcd);
+            if (lcd != null) {
+                lcd.ContentType = ContentType.TEXT_AND_IMAGE;
+            }
+
+            gts.initListByTag("control", controlRotors);
+            g.persist("control rotors found " + controlRotors.Count);
+
+            IMyMotorAdvancedStator first = null;
+            gts.getByTag("arm", ref first);
+            var finger = new Finger(first, g, true);
+            if (finger.okay) {
+                fingers.Add(finger);
+                firstFinger = finger;
+                //g.persist(g.gps("firstFinger", firstFinger.hinge.WorldMatrix.Translation));
+            } else {
+                walkComplete = true;
+                g.persist("firstFinger not okay");
+            }
+            gts.getByTag("control", ref control);
+        }
+
+        Vector3D target = Vector3D.Zero;
+        void doWalk() {
+            Finger finger = null;
+            if (!walkComplete) {
+                if (lastFinger == null) {
+                    finger = firstFinger.next();
+                } else {
+                    finger = lastFinger.next();
+                }
+                if (finger == null) {
+                    walkComplete = true;
+                } else {
+                    if (finger.okay) {
+                        fingers.Add(finger);
+                        lastFinger = finger;
+                    }
+                }
+            } else {
+                g.log("walk complete found ", fingers.Count, " fingers");
+                if (firstFinger != null) {
+                    firstFinger.info();
+                }
+            }
+        }
+        float angle = 0;
+        void procArgument(string arg) {
+            float val;
+            if (arg == "pi") {
+                angle = MathHelper.Pi;
+            } else if (arg == "2pi") {
+                angle = MathHelper.TwoPi;
+            } else if (float.TryParse(arg, out val)) {
+                MathHelper.LimitRadians(ref val);
+                angle = val;
+            }
+            g.log("target angle ", angle);
+        }
+        V3DLag targetLag = new V3DLag(6);
+        public void Main(string argument, UpdateType updateSource) {
+            try {
+                var bcontrol = control != null;
+                procArgument(argument);
+                doWalk();
+                if (controlRotors.Count > 0 && control != null) {
+                    g.log("control okay");
+                    var target = Vector3D.Zero;
+                    IMyEntity tip = null;
+                    if (lastFinger == null) {
+                        if (firstFinger.okay) {
+                            tip = firstFinger.tip;
+                            if (tip == null) {
+                                tip = firstFinger.hinge;
+                            }
+                        }
+                    } else {
+                        if (lastFinger.okay) {
+                            tip = lastFinger.tip;
+                            if (tip == null) {
+                                tip = lastFinger.hinge;
+                            }
+                        }
+                    }
+                    if (tip != null) {
+                        target = tip.WorldMatrix.Translation;
+                    }
+                    
+                    foreach (var r in controlRotors) {
+                        if (target == Vector3D.Zero) {
+                            r.TargetVelocityRad = 0;
+                        } else {
+                            var dif = control.WorldMatrix.Translation - r.WorldMatrix.Translation;
+                            pointRotoAtTarget(r, targetLag.update(target - dif));
+                        }
+                        //pointRotoAtTarget(r, finger.WorldMatrix.Translation);
+                    }
+                }
+                foreach (var f in fingers) {
+                    //f.zero();
+                    f.setHingeAngle(0.5f);
+                    f.setRotorAngle(angle);
+                }
+            } catch (Exception ex) {
+                g.persist(ex.ToString());
+            }
+            var str = g.clear();
+            if (lcd != null) {
+                lcd.WriteText(str);
+            }
+            Echo(str);
+        }
+        void pointRotoAtTarget(IMyMotorStator aRoto, Vector3D aTarget) {
+            // cos(angle) = dot(vecA, vecB) / (len(vecA)*len(vecB))
+            // angle = acos(dot(vecA, vecB) / (len(vecA)*len(vecB)))
+            // angle = acos(dot(vecA, vecB) / sqrt(lenSq(vecA)*lenSq(vecB)))
+            if (aTarget == Vector3D.Zero) {
+                pointRotoAtDirection(aRoto, aTarget);
+            } else {
+                var matrix = aRoto.WorldMatrix;
+                var projectedTarget = aTarget - Vector3D.Dot(aTarget - matrix.Translation, matrix.Up) * matrix.Up;
+                var projectedDirection = Vector3D.Normalize(matrix.Translation - projectedTarget);
+                pointRotoAtDirection(aRoto, projectedDirection);
+            }
+        }
+        void pointRotoAtDirection(IMyMotorStator aRoto, Vector3D aDirection) {
+            if (null != aRoto) {
+                if (Vector3D.Zero == aDirection) {
+                    aRoto.TargetVelocityRad = 0;
+                    return;
+                }
+                var matrix = aRoto.WorldMatrix;
+                double dot;
+                
+                var angle = MAF.angleBetween(aDirection, matrix.Forward, out dot);
+                //log("roto angle ", angle);
+                double targetAngle;
+                var v = 0.0;
+                if (!double.IsNaN(angle)) {
+                    // norm = dir to me cross grav
+                    // dot = dir to obj dot norm
+                    var norm = aDirection.Cross(matrix.Forward);
+
+                    dot = matrix.Up.Dot(norm);
+                    if (dot < 0) {
+                        targetAngle = (Math.PI * 2) - angle;
+                    } else {
+                        targetAngle = angle;
+                    }
+                    v = targetAngle - aRoto.Angle;
+                    if (v > Math.PI) {
+                        v -= (Math.PI * 2);
+                    }
+                    if (v < -Math.PI) {
+                        v += (Math.PI * 2);
+                    }
+                }
+                aRoto.Enabled = true;
+                aRoto.RotorLock = false;
+                var max = 0.2;
+                aRoto.TargetVelocityRad = (float)MathHelper.Clamp(v, -max, max);
+            }
+        }
+    }
+}
---
+++ Library/BOX.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VRageMath;
+
+namespace IngameScript
+{
+    /// <summary>
+    /// kbox = 1000m^3 
+    /// cbox = 100m^3 and has an index position within the k box from 0 to 999 
+    /// kbox exists in the world
+    /// cbox exists in the kbox
+    /// </summary>
+    static class BOX
+    {
+        static readonly Vector3D[] points = new Vector3D[2];
+        const int cmax = 10;
+        public const double cdist = 173.205080756888;
+        /// <summary>
+        /// Returns the bounding K box in world coordinates
+        /// </summary>
+        /// <param name="aWorldPosition"></param>
+        /// <returns></returns>
+        public static BoundingBoxD GetKBox(Vector3D aWorldPosition) {
+            var k = WorldToK(aWorldPosition);
+            var v = KToWorld(k);
+            aWorldPosition = new Vector3D(1000);
+            if (v.X < 0)
+                aWorldPosition.X = -1000;
+            if (v.Y < 0)
+                aWorldPosition.Y = -1000;
+            if (v.Z < 0)
+                aWorldPosition.Z = -1000;
+            points[0] = v;
+            points[1] = v + aWorldPosition;
+            return BoundingBoxD.CreateFromPoints(points);
+        }
+        /// <summary>
+        /// Returns the bounding C box in world coordinates
+        /// </summary>
+        /// <param name="aWorldPosition"></param>
+        /// <returns></returns>
+        public static BoundingBoxD GetCBox(Vector3D aWorldPosition) {
+            var k = KToWorld(WorldToK(aWorldPosition));
+            var v2cresult = KToC(aWorldPosition - k);
+            var c = k + CToK(v2cresult);
+            k = new Vector3D(100);
+            if (c.X < 0)
+                k.X = -100;
+            if (c.Y < 0)
+                k.Y = -100;
+            if (c.Z < 0)
+                k.Z = -100;
+            points[0] = c;
+            points[1] = c + k;
+            return BoundingBoxD.CreateFromPoints(points);
+        }
+        /// <summary>
+        /// Returns the index of the cbox for the given cbox position
+        /// </summary>
+        /// <param name="v">value from </param>
+        /// <returns></returns>
+        public static int CVectorToIndex(Vector3D v) => ((int)v.Z * cmax * cmax) + ((int)v.Y * cmax) + (int)v.X;
+        public static Vector3D CIndexToVector(int idx) {
+            int z = idx / (cmax * cmax);
+            idx -= (z * cmax * cmax);
+            int y = idx / cmax;
+            int x = idx % cmax;
+            return new Vector3D(x, y, z);
+        }
+        /// <summary>
+        /// k resolution boxing
+        /// </summary>
+        /// <param name="v">World position</param>
+        /// <returns></returns>
+        public static Vector3D WorldToK(Vector3D v) => v2n(v, 1000);
+        /// <summary>
+        /// k resolution unboxing
+        /// </summary>
+        /// <param name="v">KBox position</param>
+        /// <returns></returns>
+        public static Vector3D KToWorld(Vector3D v) => n2v(v, 1000);
+        /// <summary>
+        /// Returns the world position that is the same relative position in the adjacent cbox 
+        /// that is closest to the provided direction, uses base6 directions only
+        /// intended to receive a cbox center as the aWorldPosition but this is not necessary
+        /// </summary>
+        /// <param name="aWorldPosition">position in the world</param>
+        /// <param name="aWorldDirection">unit vector direction in world space</param>
+        /// <returns></returns>
+        public static Vector3D MoveC(Vector3D aWorldPosition, Vector3D aWorldDirection) => MoveN(aWorldPosition, aWorldDirection, 100.0);
+        /// <summary>
+        /// Returns the world position that is the same relative position in the adjacent kbox 
+        /// that is closest to the provided direction, uses base6 directions only
+        /// intended to receive a kbox center as the aWorldPosition but this is not necessary
+        /// </summary>
+        /// <param name="aWorldPosition">position in the world</param>
+        /// <param name="aWorldDirection">unit vector direction in world space</param>
+        /// <returns></returns>
+        public static Vector3D MoveK(Vector3D aWorldPosition, Vector3D aWorldDirection) => MoveN(aWorldPosition, aWorldDirection, 1000.0);
+        public static BoundingBoxD moveTowardsDir(BoundingBoxD aBox, Vector3D aDir) {
+            var disp = aBox.Max - aBox.Min;
+            var mag = disp.Length();
+            points[0] = aBox.Min + (aDir * mag);
+            points[1] = aBox.Max + (aDir * mag);
+            return BoundingBoxD.CreateFromPoints(points);
+        }
+        public static BoundingBoxD moveTowardsPos(BoundingBoxD aBox, Vector3D aTarget) {
+            var disp = aBox.Max - aBox.Min;
+            var mag = disp.Length() * 0.75;
+            var dir = Vector3D.Normalize(aTarget - aBox.Center);
+            points[0] = aBox.Min + (dir * mag);
+            points[1] = aBox.Max + (dir * mag);
+            return BoundingBoxD.CreateFromPoints(points);
+        }
+        static Vector3D MoveN(Vector3D aWorldPosition, Vector3D aWorldDirection, double n) {
+            int d = (int)Base6Directions.GetClosestDirection((Vector3)aWorldDirection);
+            Vector3D dir = Base6Directions.Directions[d];
+            aWorldPosition.X += dir.X * n;
+            aWorldPosition.Y += dir.Y * n;
+            aWorldPosition.Z += dir.Z * n;
+            return aWorldPosition;
+        }
+        /// <summary>
+        /// C resolution boxing, previous v2c
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        static Vector3D KToC(Vector3D v) => v2n(v, 100);
+        /// <summary>
+        /// C resolution unboxing, previous c2v
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        static Vector3D CToK(Vector3D v) => n2v(v, 100);
+        /// <summary>
+        /// variable resolution unboxing
+        /// </summary>
+        /// <param name="v">Position</param>
+        /// <param name="n">resolution</param>
+        /// <returns></returns>
+        static Vector3D n2v(Vector3D v, long n) {
+            if (v.X < 0) {
+                v.X++;
+            }
+            if (v.Y < 0) {
+                v.Y++;
+            }
+            if (v.Z < 0) {
+                v.Z++;
+            }
+            v.X *= n;
+            v.Y *= n;
+            v.Z *= n;
+            return v;
+        }
+        /// <summary>
+        /// variable resolution boxing
+        /// </summary>
+        /// <param name="v">Position</param>
+        /// <param name="n">Resolution</param>
+        /// <returns></returns>
+        static Vector3D v2n(Vector3D v, long n) {
+            if (v.X < 0) {
+                v.X -= n;
+            }
+            if (v.Y < 0) {
+                v.Y -= n;
+            }
+            if (v.Z < 0) {
+                v.Z -= n;
+            }
+            v.X = (long)v.X / n;
+            v.Y = (long)v.Y / n;
+            v.Z = (long)v.Z / n;
+            return v;
+        }
+    }
+}
